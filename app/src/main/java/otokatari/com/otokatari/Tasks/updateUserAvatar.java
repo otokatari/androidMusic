a59518aa91b64c4e7d34605afd4b8d7a8b2aadf7
poxiaozheng <277492866@qq.com>
2019-07-25 23:33:40 +0800

    完成更新用户头像的接口

---
+++ app/src/main/java/otokatari/com/otokatari/Tasks/updateUserAvatar.java
@@ -0,0 +1,31 @@
+package otokatari.com.otokatari.Tasks;
+
+import okhttp3.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class updateUserAvatar {
+    public void updateUserAvatars()throws IOException {
+        String url = "http://129.204.223.158/backend/user/profile/changeavatar";
+        File file = new File("avatar.jpg");
+        String ext = file.getName().split(".")[1];
+        RequestBody fileBody = RequestBody.create(MediaType.parse("image/"+ext),file);
+        RequestBody body = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("file",file.getName(),fileBody)
+                .build();
+        Map<String,String> headers = new HashMap<>();
+        headers.put("Authorization","Bearer "+"<JWT-Token-Here>");
+        Headers headerz = Headers.of(headers);
+        Request request = new Request.Builder().post(body).headers(headerz).url(url).build();
+        OkHttpClient client = new OkHttpClient();
+        Response resp = client.newCall(request).execute();
+        if(resp.isSuccessful())
+        {
+            System.out.println(resp.body().string());
+        }
+    }
+}
